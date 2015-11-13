695dc2731a5fab5d600a365c4bbb335ed22a6f99
Vladislav <v.kirbaba@gmail.com>
2015-11-13 13:46:35 +0300

    Домашняя. Конец

--- footer.php
+++ footer.php
@@ -1,3 +1,51 @@
+<footer class="footer">
+	<div class="contain">
+		
+		<ul class="footer__regions">
+			<h3>Регионы</h3>
+			<li><a href="#">Алтай</a></li>
+			<li><a href="#">Байкал</a></li>
+			<li><a href="#">Вьетнам</a></li>
+			<li><a href="#">Греция</a></li>
+			<li><a href="#">Исландия</a></li>
+			<li><a href="#">Кавказ</a></li>
+			<li><a href="#">Камчатка</a></li>
+			<li><a href="#">Крым</a></li>
+			<li><a href="#">Тибет</a></li>
+			<li><a href="#">Южная Америка</a></li>
+		</ul>
+		
+		<ul class="footer__sitemap">
+			<h3>Клуб путешественников</h3>
+			<li><a href="#">Главная</a></li>
+			<li><a href="#">Регионы</a></li>
+			<li><a href="#">О нас</a></li>
+			<li><a href="#">Поиск туров</a></li>
+			<li><a href="#">Блог</a></li>
+			<li><a href="#">Контакты</a></li>
+			<li><a href="#">Расписание туров</a></li>
+		</ul>
+		<div class="footer__contacts">
+			<h3><span>Контакты</span></h3>
+			<p class="footer__contacts--title">Email:</p>
+			<p><b>nazvanie@gmail.com</b></p>
+			<p class="footer__contacts--title">Телефон:</p>
+			<p><b>8 800 333 33 33</b></p>
+			<p><b>1 111 111 11 11</b></p>
+			<div class="footer__support">
+				<a href="#">Подписаться на новости клуба</a>
+			</div>			
+		</div>
+
+	</div>
+</footer>
+
+<article class="footer__copyright">
+	<div class="contain">
+		<p class="align-left">© Copyright 2015. All Rights Reserved.</p>
+		<a  href="#" class="align-right">Дмитрий Завадский</p>
+	</div>
+</article>
 <?php wp_footer(); ?>
 </body>
 </html>
